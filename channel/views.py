343678994f23ac6ea8bd9930a1d134632e60ed3d
mojtaba <mojtabanafez96@gmail.com>
2021-05-04 19:12:24 +0430

    create random link api

--- channel/views.py
+++ channel/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+import threading
 
-# Create your views here.
+from rest_framework import status, exceptions
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from django.db.models import Q
+from rest_framework.views import APIView
+
+from User.models import UserProfile
+from .serializers import *
+from .models import *
+
+create_link_lock = threading.Lock()
+
+
+def create_uuid_link(thread_lock):
+    import uuid
+    with thread_lock:
+        return uuid.uuid4().hex
+
+
+class CreateLinkAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            unique_link = create_uuid_link(create_link_lock)
+            return Response(data={
+                "invite_link": unique_link,
+            }, status=status.HTTP_200_OK)
+        except Exception as server_error:
+            return Response(server_error.__str__(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
