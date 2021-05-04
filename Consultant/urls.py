19457bd7b4dc089c00260fda99b68079ae92188a
github_username <hamidreza.azarbad77@gmail.com>
2021-05-04 18:24:04 +0430

    add channel message urls

--- Consultant/urls.py
+++ Consultant/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from User import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('channel-message/<int:channelId>/', include('message.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
