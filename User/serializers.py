c209d8e7beb28b9ad6ea747269e4bd3275a95f1a
github_username <hamidreza.azarbad77@gmail.com>
2021-05-04 18:02:48 +0430

    add user, consultant serializers

---
+++ User/serializers.py
@@ -0,0 +1,68 @@
+from abc import ABC
+
+from rest_framework import serializers
+from .models import *
+
+
+class LoginSerializer(serializers.Serializer):
+    email_username = serializers.CharField(required=True, allow_blank=False, allow_null=False)
+    password = serializers.CharField(required=True, allow_null=False, allow_blank=False, min_length=6)
+
+
+class BaseUserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True, allow_null=False)
+    username = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=128)
+    avatar = serializers.FileField(allow_empty_file=True, allow_null=True, required=False)
+    email = serializers.EmailField(required=True, allow_blank=False, allow_null=False)
+    first_name = serializers.CharField(required=True, allow_null=False, allow_blank=False)
+    last_name = serializers.CharField(required=True, allow_null=False, allow_blank=False)
+    phone_number = serializers.CharField(required=True, allow_blank=False, allow_null=False)
+    password = serializers.CharField(required=True, allow_null=False, allow_blank=False, min_length=6, max_length=25,
+                                     write_only=True)
+    private_profile = serializers.BooleanField(default=False, allow_null=False)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.save()
+        return instance
+
+    def validate_phone_number(self, phone_number):
+        """
+        Check the phone_number regex.
+        """
+        import re
+        if len(phone_number) != 11 or re.search(r"09[0-9]{9}", phone_number) is None:
+            raise serializers.ValidationError("Format of phone_number is not true")
+        return phone_number
+
+
+class UserProfileSerializer(BaseUserSerializer):
+    user_type = serializers.CharField(read_only=True, allow_null=False, allow_blank=False)
+
+    def create(self, validated_data):
+        return UserProfile.objects.create(**validated_data)
+
+
+class ConsultantProfileSerializer(BaseUserSerializer):
+    certificate = serializers.FileField(required=True, allow_null=False, allow_empty_file=False)
+    consultant_types = (
+        ('Lawyer', 'Lawyer'),
+        ('medical', 'medical'),
+        ('EntranceExam', 'EntranceExam'),
+        ('Psychology', 'Psychology'),
+        ('Immigration', 'Immigration'),
+        ('AcademicAdvice', 'AcademicAdvice')
+    )
+    user_type = serializers.ChoiceField(choices=consultant_types, required=True)
+
+    def create(self, validated_data):
+        return ConsultantProfile.objects.create(**validated_data)
+
+    def validate_certificate(self, certificate_file):
+        # TODO CHECK CERTIFICATE EXTENSION
+        return certificate_file
