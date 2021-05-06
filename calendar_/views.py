4679ad8366d3ecb379c25d85808083d6257d003a
github_username <hamidreza.azarbad77@gmail.com>
2021-05-06 18:35:40 +0430

    write define_hour_api

--- calendar_/views.py
+++ calendar_/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
 
-# Create your views here.
+
+class ConsultantTime(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            consultant_time_serializer = ConsultantTimeSerializer(data=request.data)
+            if consultant_time_serializer.is_valid():
+                if consultant_time_serializer.validated_data.contains('consultant_id'):
+                    consultant = ConsultantProfile.objects.filter(
+                        id=consultant_time_serializer.validated_data['consultant_id'])
+                    if len(consultant) == 0:
+                        return Response({"error": "مشاوری با این شناسه موجود نیست"}, status=status.HTTP_400_BAD_REQUEST)
+                    else:
+                        consultant = consultant[0]
+                        secretary = UserProfile.objects.filter(baseuser_ptr = request.user)[0]
+                        if len(ConsultantProfile.my_secretaries.through.objects.filter(consultantprofile=consultant, userprofile=secretary)) == 0:
+                            return Response({"error": "شما منشی این مشاور نیستید"}, status=status.HTTP_403_FORBIDDEN)
+                else:
+                    consultant = ConsultantProfile.objects.filter(baseuser_ptr=request.user)
+                    if len(consultant) == 0:
+                        return Response({"error": "شما مشاور نیستید"}, status=status.HTTP_403_FORBIDDEN)
+                    else:
+                        consultant = consultant[0]
+
+                consultant_time_serializer.validated_data['consultant'] = consultant
+                consultant_time = consultant_time_serializer.save()
+                return Response(ConsultantTimeSerializer(consultant_time).data, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": consultant_time_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as server_error:
+            return Response({"error": server_error.__str__()}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
