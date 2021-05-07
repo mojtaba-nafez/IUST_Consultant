57a67e84482e1f238412df2851e08c4de719827c
github_username <hamidreza.azarbad77@gmail.com>
2021-05-07 18:29:26 +0430

    write url for update consultant time

--- calendar_/urls.py
+++ calendar_/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('consultant-time/', views.ConsultantTimeAPI.as_view(), name='crud_consultant_time'),
+    path('consultant-time/', views.ConsultantTimeAPI.as_view(), name='cr_consultant_time'),
+    path('consultant-time/<int:ConsultantTimeId>/', views.ConsultantTimeAPI.as_view(), name='ud_consultant_time'),
 ]
