beea5e360c02fb9c4f34ef59a2d0f74faee1fff5
github_username <hamidreza.azarbad77@gmail.com>
2021-05-18 15:02:29 +0430

    restore user channels and user role api

--- User/user_urls.py
+++ User/user_urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-
+from channel.views import UserChannelsAPI
 
 urlpatterns = [
     path('signup/', views.UserSignupAPI.as_view(), name='user_signup_api'),
     path('login/', views.LoginAPI.as_view(), name='user_login_api'),
     path('logout/', views.LogoutAPI.as_view(), name='user_logout_api'),
+    path('channels/', UserChannelsAPI.as_view(), name='user channels'),
+    path('channel-role/<int:ChannelId>/', UserChannelsAPI.as_view(), name='user channels'),
 ]
