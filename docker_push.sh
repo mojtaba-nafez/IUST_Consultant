fbd6fbfc143c057f3984230088d4dc57a1122980
mojtaba <mojtabanafez96@gmail.com>
2021-05-19 00:43:02 +0430

    test travic ci

--- docker_push.sh
+++ docker_push.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-sudo docker login --username "mojtabanafez96@gmail.com" --password "b7a69d7c-73a7-4edd-bb12-2c0b52a2094f" registry.heroku.com
-sudo docker tag consultant-iust:latest registry.heroku.com/consultant-iust/web
-if [ $TRAVIS_BRANCH == "Dockerization" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/consultant-se-iust/consultant; fi
-
-chmod +x heroku-container-release.sh
-sudo chown $USER:docker ~/.docker
-sudo chown $USER:docker ~/.docker/config.json
-sudo chmod g+rw ~/.docker/config.json
-
-./heroku-container-release.sh
+sudo docker login --username "mojtabanafez96@gmail.com" --password "c72cc697-deaf-4e4a-9212-582913749385" registry.heroku.com
+heroku container:push web -a consultant-iust && heroku container:release web -a consultant-iust
