c7250ba92e5144b593e62a63efa3f7abc6eddbee
github_username <hamidreza.azarbad77@gmail.com>
2021-05-24 14:38:45 +0430

    write base of consumer

--- Consultant/asgi.py
+++ Consultant/asgi.py
@@ -1,13 +1,20 @@
 import os
 
 import django
+import chat_room.routing
+from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Consultant.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
   "http": AsgiHandler(),
+  "websocket": AuthMiddlewareStack(
+    URLRouter(
+      chat_room.routing.websocket_urlpatterns
+    )
+  ),
   # Just HTTP for now. (We can add other protocols later.)
 })
