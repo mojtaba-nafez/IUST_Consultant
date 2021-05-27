d2976c2ba9bb5a27d25a5b1f9b086900475cd90a
github_username <hamidreza.azarbad77@gmail.com>
2021-05-27 10:48:03 +0430

    write base of consumer for chat

--- Consultant/settings.py
+++ Consultant/settings.py
@@ -86,6 +86,28 @@ TEMPLATES = [
 # Channels
 ASGI_APPLICATION = 'Consultant.asgi.application'
 
+#comunication for consumers(channels)
+CHANNEL_LAYERS = {
+    'default': {
+        ### Method 1: Via redis lab
+        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        # 'CONFIG': {
+        #     "hosts": [
+        #       'redis://h:<password>;@<redis Endpoint>:<port>'
+        #     ],
+        # },
+
+        ### Method 2: Via local Redis
+        # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        # 'CONFIG': {
+        #      "hosts": [('127.0.0.1', 6379)],
+        # },
+
+        ### Method 3: Via In-memory channel layer
+        ## Using this method.
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
--- chat_room/consumers.py
+++ chat_room/consumers.py
@@ -1,18 +1,44 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
+class DirectConsumer(JsonWebsocketConsumer):
+
     def connect(self):
+        self.room_group_name = 'direct_%s' % self.scope['url_route']['kwargs']['room_name']
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+    # Receive message from WebSocket
+    def receive_json(self, content, **kwargs):
+        message = content['message']
 
-        self.send(text_data=json.dumps({
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        self.send_json(content={
             'message': message
-        }))
+        })
