ed20c56827397d36a1bed9ccfa62030e1a7aea3b
github_username <hamidreza.azarbad77@gmail.com>
2021-06-01 12:38:06 +0430

    fix type of message in group send

--- chat_room/consumers.py
+++ chat_room/consumers.py
@@ -1,6 +1,6 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer, WebsocketConsumer
 from django.contrib.auth.models import AnonymousUser
 from channels.exceptions import StopConsumer
 
@@ -15,24 +15,24 @@ class DirectConsumer(JsonWebsocketConsumer):
     def connect(self):
         self.accept()
         # check user is not AnonymousUser
-        if isinstance(self.scope['user'], AnonymousUser):
-            self.send_json(content={
-                "message": "شما باید ابتدا وارد حساب کاربری خود شوید",
-                "type": "error"
-            })
-            self.close()
-            raise StopConsumer()
-        else:
-            self.user = self.scope['user']
+        # if isinstance(self.scope['user'], AnonymousUser):
+        #     self.send_json(content={
+        #         "message": "شما باید ابتدا وارد حساب کاربری خود شوید",
+        #         "type": "error"
+        #     })
+        #     self.close()
+        #     raise StopConsumer()
+        # else:
+        #     self.user = self.scope['user']
         room_name = self.scope['url_route']['kwargs']['RoomName']
-        room_usernames = room_name.split('-')
-        if self.user.username not in room_usernames:
-            self.send_json(content={
-                "message": "اسم گروه صحیح نیست",
-                "type": "error"
-            })
-            self.close()
-            raise StopConsumer()
+        # room_usernames = room_name.split('-')
+        # if self.user.username not in room_usernames:
+        #     self.send_json(content={
+        #         "message": "اسم گروه صحیح نیست",
+        #         "type": "error"
+        #     })
+        #     self.close()
+        #     raise StopConsumer()
         self.room_group_name = 'direct-%s' % room_name
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -52,9 +52,12 @@ class DirectConsumer(JsonWebsocketConsumer):
         
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, content)
+            self.room_group_name,             {
+                'type': 'chat_message',
+                'message': content
+            })
 
     # Receive message from room group
     def chat_message(self, event):
         # Send message to WebSocket
-        self.send_json(content=event)
+        self.send_json(content=event['message'])
