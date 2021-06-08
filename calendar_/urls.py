929a905b5cbc12e2f06382767b5313bbd33afc2f
github_username <hamidreza.azarbad77@gmail.com>
2021-06-09 00:47:31 +0430

    write url for comment

--- calendar_/urls.py
+++ calendar_/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('consultant-time/', views.ConsultantTimeAPI.as_view(), name='cr_consultant_time'),
     path('consultant-time/<int:ConsultantTimeId>/', views.ConsultantTimeAPI.as_view(), name='ud_consultant_time'),
     path('consultant-time/cancel/<int:ConsultantTimeId>/', views.CancelConsultantTime.as_view(), name='cancel_consultant_time'),
+    path('consultant-time/comment/<int:ConsultantTimeId>/', views.CommentAndGradeAPI.as_view(), name='create comment'),
+    path('consultant-time/comment/<string:ConsultantId>/', views.CancelConsultantTime.as_view(), name='get consultant comments'),
     path('reserve/<int:ConsultantID>/', views.Reserve.as_view(), name='consultant_time'),
 ]
 
