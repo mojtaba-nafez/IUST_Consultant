fcfd33fb7499457f15cadb3dfa65aced92180ed9
github_username <hamidreza.azarbad77@gmail.com>
2021-06-11 18:33:46 +0430

    change model for whereby links

--- calendar_/models.py
+++ calendar_/models.py
@@ -15,3 +15,7 @@ class ConsultantTime(models.Model):
     user_grade = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)], null=True, blank=True)
     user_grade_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     user_comment = models.TextField(max_length=500, null=True, blank=True)
+
+    whereby_meeting_id = models.IntegerField(null=True, blank=True, )
+    whereby_room_url = models.CharField(null=True, blank=True, max_length=500)
+    whereby_host_room_url = models.CharField(null=True, blank=True, max_length=500)
--- video_chat/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class VideoChatRequestSerializer(serializers.Serializer):
-    meetingId = serializers.IntegerField(read_only=True, allow_null=False)
